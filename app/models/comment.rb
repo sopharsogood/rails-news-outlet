d60e7d15e330e60f2d65a6d50c4624a27e7f2d86
sophie <justanothersophie@gmail.com>
2021-09-16 16:20:21 -0400

    Comment replies renamed to children

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,4 +8,7 @@ class Comment < ApplicationRecord
 
     belongs_to :user
     belongs_to :article
+
+    has_many :children, class_name: "Comment", foreign_key: "parent_id"
+    belongs_to :parent, class_name: "Comment", optional: true
 end
