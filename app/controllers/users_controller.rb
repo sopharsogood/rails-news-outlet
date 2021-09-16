8a3f7a3b11271061a8f31cc1ea07e38626d50545
sophie <justanothersophie@gmail.com>
2021-09-16 19:40:38 -0400

    login routes/actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def login
+        @user = User.new
+    end
+
+    def signin
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            @user = User.new unless @user
+            render :login
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :articles
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to 'users#login'
+  post '/login', to 'users#signin'
 end
