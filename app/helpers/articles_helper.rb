0995d74a8316b63163f6c8cdd67ede55979e3347
sophie <justanothersophie@gmail.com>
2021-09-16 18:06:32 -0400

    open and close h tag helper methods so top three stories are emphasized

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,17 @@
 module ArticlesHelper
+    def open_h_tag(index)
+        if index < 3
+            "<h2>"
+        else
+            "<h3>"
+        end
+    end
+
+    def close_h_tag(index)
+        if index < 3
+            "</h2>"
+        else
+            "</h3>"
+        end
+    end
 end
