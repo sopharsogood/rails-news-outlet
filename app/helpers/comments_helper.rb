92ac612cdc4b84005a1454c5e7b947648e6322cb
sophie <justanothersophie@gmail.com>
2021-09-16 17:40:11 -0400

    comment show view helper methods

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -6,4 +6,20 @@ module CommentsHelper
             7
         end
     end
+
+    def parent_or_self_if_orphan(comment)
+        if comment.parent_id
+            comment.parent
+        else
+            comment
+        end
+    end
+
+    def self_if_parent_or_children_otherwise(comment)
+        if comment.parent_id
+            [comment]
+        else
+            comment.children
+        end
+    end
 end
