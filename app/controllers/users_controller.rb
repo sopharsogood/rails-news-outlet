25ec5c966fd6daa7a8f916e131fd7b68ca43586a
sophie <justanothersophie@gmail.com>
2021-09-16 20:42:18 -0400

    user new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(new_user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            render :login
+        end
+    end
+
+
     def login
         @user = User.new
     end
@@ -13,6 +28,12 @@ class UsersController < ApplicationController
             @user = User.new unless @user
             render :login
         end
+    end
+
+    private
+
+    def new_user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
