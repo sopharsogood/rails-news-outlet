ff971e89e5c19f8323ef727e3fafc7cbaf4d59d9
sophie <justanothersophie@gmail.com>
2021-09-16 22:03:08 -0400

    reject_if_comment_does_not_match_article controller method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+    private
+
+    def reject_if_comment_does_not_match_article
+        article = Article.find_by(id: params[:article_id])
+        comment = Comment.find_by(id: params[:id])
+        if !article
+            flash[:error] = "No such article found"
+            redirect_to articles_path
+            true
+        elsif !comment || comment.article_id != article.id
+            flash[:error] = "No such comment found on this article"
+            redirect_to article
+            true
+        else
+            false
+        end
+    end
 end
