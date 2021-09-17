eb2bdd1a87cf82916c8975bce8d14c8803b6edb2
sophie <justanothersophie@gmail.com>
2021-09-16 22:51:17 -0400

    comment controller new and create actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,20 @@
 class CommentsController < ApplicationController
 
+    def new
+        reject_if_comment_does_not_match_article_but_allow_absent_comment
+    end
+
+    def create
+        unless reject_if_comment_does_not_match_article_but_allow_absent_comment
+            @comment = Comment.new(new_comment_params)
+            if @comment.save
+                redirect_to article_comment_path(@article, @comment)
+            else
+                render :new
+            end
+        end
+    end
+
     def edit
         reject_if_comment_does_not_match_article
     end
@@ -24,6 +39,22 @@ class CommentsController < ApplicationController
             redirect_to articles_path
             true
         elsif !@comment || @comment.article_id != article.id
+            flash[:error] = "No such comment found on this article"
+            redirect_to article
+            true
+        else
+            false
+        end
+    end
+
+    def reject_if_comment_does_not_match_article_but_allow_absent_comment
+        @article = Article.find_by(id: params[:article_id])
+        @comment = Comment.find_by(id: params[:article_id])
+        if !@article
+            flash[:error] = "No such article found"
+            redirect_to articles_path
+            true
+        elsif @comment && @comment.article_id != article.id
             flash[:error] = "No such comment found on this article"
             redirect_to article
             true
