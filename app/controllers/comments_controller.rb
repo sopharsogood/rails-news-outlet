4b181ab16557334301c65843528d5470f26d4a4c
sophie <justanothersophie@gmail.com>
2021-09-17 03:16:45 -0400

    Comments controller reworked to validate paths as before_action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,74 +1,40 @@
 class CommentsController < ApplicationController
+    before_action :redirect_if_invalid_article_route
+
+    before_action :redirect_if_comment_not_found_for_article
+    skip_before_action :redirect_if_comment_not_found_for_article, only: [:new, :create]
+
     before_action :redirect_if_not_logged_in
     skip_before_action :redirect_if_not_logged_in, only: [:show]
 
     def new
-        reject_if_comment_does_not_match_article_but_allow_absent_comment
         @comment = Comment.new(article: @article, parent: @comment, user: current_user)
     end
 
     def create
-        unless reject_if_comment_does_not_match_article_but_allow_absent_comment
-            @comment = Comment.new(new_comment_params)
-            if @comment.save
-                redirect_to article_comment_path(@article, @comment)
-            else
-                render :new
-            end
+        @comment = Comment.new(new_comment_params)
+        if @comment.save
+            redirect_to article_comment_path(@article, @comment)
+        else
+            render :new
         end
     end
 
     def edit
-        reject_if_comment_does_not_match_article
     end
 
     def update
-        unless reject_if_comment_does_not_match_article
-            if @comment.update(edit_comment_params)
-                redirect_to @comment
-            else
-                render :edit
-            end
+        if @comment.update(edit_comment_params)
+            redirect_to @comment
+        else
+            render :edit
         end
     end
 
     def show
-        reject_if_comment_does_not_match_article
     end
 
     private
-
-    def reject_if_comment_does_not_match_article
-        @article = Article.find_by(id: params[:article_id])
-        @comment = Comment.find_by(id: params[:id])
-        if !@article
-            flash[:error] = "No such article found"
-            redirect_to articles_path
-            true
-        elsif !@comment || @comment.article_id != @article.id
-            flash[:error] = "No such comment found on this article"
-            redirect_to article
-            true
-        else
-            false
-        end
-    end
-
-    def reject_if_comment_does_not_match_article_but_allow_absent_comment
-        @article = Article.find_by(id: params[:article_id])
-        @comment = Comment.find_by(id: params[:article_id])
-        if !@article
-            flash[:error] = "No such article found"
-            redirect_to articles_path
-            true
-        elsif @comment && @comment.article_id != @article.id
-            flash[:error] = "No such comment found on this article"
-            redirect_to article
-            true
-        else
-            false
-        end
-    end
 
     def edit_comment_params
         params.require(:comment).permit(:content)
@@ -76,5 +42,21 @@ class CommentsController < ApplicationController
 
     def new_comment_params
         params.require(:comment).permit(:content, :user_id, :parent_id, :article_id)
+    end
+
+    def redirect_if_invalid_article_route
+        @article = Article.find_by(id: params[:article_id])
+        if !@article
+            flash[:error] = "No such article found."
+            redirect_to articles_path
+        end
+    end
+
+    def redirect_if_comment_not_found_for_article
+        @comment = @article.comments.find_by(id: params[:id])
+        if !@comment
+            flash[:error] = "No such comment found on this article."
+            redirect_to @article
+        end
     end
 end
