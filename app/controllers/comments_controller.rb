c3f8ff3789533f8fc8201b9f1fa570b5d9423797
sophie <justanothersophie@gmail.com>
2021-09-16 22:38:41 -0400

    comment controller edit and update actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,38 @@
 class CommentsController < ApplicationController
 
+    def edit
+        reject_if_comment_does_not_match_article
+    end
+
+    def update
+        unless reject_if_comment_does_not_match_article
+            if @article.update(edit_article_params)
+                redirect_to @article
+            else
+                render edit_article_path(@article)
+            end
+        end
+    end
+
     private
 
     def reject_if_comment_does_not_match_article
-        article = Article.find_by(id: params[:article_id])
-        comment = Comment.find_by(id: params[:id])
-        if !article
+        @article = Article.find_by(id: params[:article_id])
+        @comment = Comment.find_by(id: params[:id])
+        if !@article
             flash[:error] = "No such article found"
             redirect_to articles_path
             true
-        elsif !comment || comment.article_id != article.id
+        elsif !@comment || @comment.article_id != article.id
             flash[:error] = "No such comment found on this article"
             redirect_to article
             true
         else
             false
         end
+    end
+
+    def edit_article_params
+        params.require(:article).permit(:title, :content)
     end
 end
