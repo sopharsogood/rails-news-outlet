ec370986077b363d27b7b41fbb6a40825d2e0690
sophie <justanothersophie@gmail.com>
2021-09-17 00:08:46 -0400

    author_byline fixed and reinstated

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -7,6 +7,10 @@ module ArticlesHelper
         end
     end
 
+    def author_byline(article)
+        link_to "by #{article.author.name}", article.author
+    end
+
     def display_article_link(article)
         link_to article.title, article
     end
