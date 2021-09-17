dcf66278196824baa3d886271783fc988ca97a36
sophie <justanothersophie@gmail.com>
2021-09-17 03:20:26 -0400

    redirect_to @comment changed to redirect_to article_comment_path(@comment.article, @comment) so it works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,7 +27,7 @@ class CommentsController < ApplicationController
 
     def update
         if @comment.update(edit_comment_params)
-            redirect_to @comment
+            redirect_to article_comment_path(@comment.article, @comment)
         else
             render :edit
         end
@@ -65,7 +65,7 @@ class CommentsController < ApplicationController
     def redirect_if_wrong_user
         unless current_user_is?(@comment.user)
             flash[:error] = "Only the original poster of a comment can edit or delete it."
-            redirect_to @comment
+            redirect_to article_comment_path(@comment.article, @comment)
         end
     end
 end
