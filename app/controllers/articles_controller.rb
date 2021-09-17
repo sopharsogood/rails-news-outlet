48cfb3c3420e2dfee26172eff699badf1f6f0166
sophie <justanothersophie@gmail.com>
2021-09-16 21:29:46 -0400

    article edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,27 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(edit_article_params)
+            redirect_to @article
+        else
+            render :edit
+        end
+    end
+
     private
 
     def new_article_params
         params.require(:article).permit(:title, :content, :author_id)
+    end
+
+    def edit_article_params
+        params.require(:article).permit(:title, :content)
     end
 
 end
