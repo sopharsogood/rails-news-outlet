9a7fd2f033cb74ff0d70ed226c0d2ccda924744a
sophie <justanothersophie@gmail.com>
2021-09-16 23:02:53 -0400

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+clark = User.create(name: "Clark Kent", password: "abc123", email: "super_good_reporting@newsrail.news", bio: "Clark Kent is a normal human who has lived on earth ever since he was born at a normal age.")
+peter = User.create(name: "Peter Parker", password: "abc123", email: "eight_eyed_camera@newsrail.news", bio: "Peter Parker is an accomplished photographer who has inexplicably never been seen taking photographs. His hobbies include cultivating his pet arachnids that strangely trip geiger counters.")
+goku = User.create(name: "420goku420", password: "abc123", email: "lol@goku.goku", bio: "did i say 420 yet")
+seph = User.create(name: "XxX_sephiroth_XxX", password: "abc123", email: "video@games")
