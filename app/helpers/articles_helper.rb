5a4aaee56f246c655c4930191fbe381629fe5bc8
sophie <justanothersophie@gmail.com>
2021-09-17 00:06:08 -0400

    article index fixed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,17 +1,9 @@
 module ArticlesHelper
-    def open_h_tag(index)
+    def h_tag_number(index)
         if index < 3
-            "<h2>"
+            "2"
         else
-            "<h3>"
-        end
-    end
-
-    def close_h_tag(index)
-        if index < 3
-            "</h2>"
-        else
-            "</h3>"
+            "3"
         end
     end
 
