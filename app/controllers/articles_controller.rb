d841fd4980d1337c5d214be33a606f7581e2b626
sophie <justanothersophie@gmail.com>
2021-09-17 03:07:15 -0400

    non-staff can't make articles, non-author can't do anything to existing articles [except comment]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
     before_action :redirect_if_not_logged_in
     skip_before_action :redirect_if_not_logged_in, only: [:show, :index]
 
+    before_action :redirect_if_not_staff_or_wrong_staff
+    skip_before_action :redirect_if_not_staff_or_wrong_staff, only: [:show, :index]
+
     def new
         @article = Article.new(author: current_user)
     end
@@ -45,5 +48,22 @@ class ArticlesController < ApplicationController
     def edit_article_params
         params.require(:article).permit(:title, :content)
     end
+
+    def redirect_if_not_staff_or_wrong_staff
+        @article = Article.find_by(id: params[:id])
+        if !current_user_is_staff?
+            flash[:error] = "Only Newsrail staff can create, edit, or delete articles."
+            if @article
+                redirect_to @article
+            else
+                redirect_to articles_path
+            end
+        elsif @article && !current_user_is?(@article.author)
+            flash[:error] = "Only the original author can edit or delete an article."
+            redirect_to @article
+        end
+    end
+
+
 
 end
