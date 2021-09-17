15b008bb38462462de546bc8cfa007d0e23b2d9f
sophie <justanothersophie@gmail.com>
2021-09-17 02:09:30 -0400

    Helper methods work in controllers; redirect_if_not_logged_in written

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    include ApplicationHelper
+
+    private
+
+    def redirect_if_not_logged_in
+        unless logged_in?
+            session[:redirected_to_login_from] = request.path
+            redirect_to '/login'
+            true
+        else
+            false #made explicit so it's clear how `unless redirect_if_not_logged_in` works
+        end
+    end
+ 
 end
