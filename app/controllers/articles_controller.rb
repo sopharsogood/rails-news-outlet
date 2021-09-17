2541a02651b223508d624f8460bc5edd51d92909
sophie <justanothersophie@gmail.com>
2021-09-16 21:27:46 -0400

    article new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+
+    def new
+        @article = Article.new(author: current_user)
+    end
+
+    def create
+        @article = Article.new(new_article_params)
+    end
+
+    private
+
+    def new_article_params
+        params.require(:article).permit(:title, :content, :author_id)
+    end
+    
 end
