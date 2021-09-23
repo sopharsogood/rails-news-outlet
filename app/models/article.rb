57ca93a9d430199ec839fee60a95b1dc1dc1bf46
sophie <justanothersophie@gmail.com>
2021-09-23 17:18:17 -0400

    recent article scope now orders articles with most recent first

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ApplicationRecord
     has_many :readings, foreign_key: :read_article_id
     has_many :readers, through: :readings
 
-    scope :recent, ->{ where('created_at >= ?', 1.week.ago) }
+    scope :recent, ->{ where('created_at >= ?', 1.week.ago).order(created_at: :desc) }
 
     validates :title, presence: true
     validates :content, presence: true
