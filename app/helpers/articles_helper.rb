e059d8b94eae26e39ce53aa8fc62d61007cb7b28
sophie <justanothersophie@gmail.com>
2021-09-26 01:03:47 -0400

    display_with_footnotes helper to make [[footnotes]] in articles possible

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -14,5 +14,20 @@ module ArticlesHelper
     def display_article_link(article)
         link_to_unless_current article.title, article
     end
+
+    def display_with_footnotes(content)
+        footnotes = ""
+        # \[\[([^\]]+)\]\] matches anything [[enclosed in double brackets]]
+        # and saves the contents of the double brackets, but not the double brackets, in $1
+        content.gsub!(\[\[([^\]]+)\]\]).with_index do |footnote, index|
+            i = index+1
+            footnotes << "<p class='footnote' id='footnote-#{i}>"
+            footnotes << "<sup><a href='#reference-#{i}'>[#{i}]</a></sup>"
+            footnotes << $1
+            footnotes << "</p>"
+            "<sup id='reference-#{i}'><a href='#footnote-#{i}'>[#{i}]</a></sup>"
+        end
+        content += footnotes
+    end
         
 end
